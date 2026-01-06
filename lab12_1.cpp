104dd930afc40d1b1d31948bb92c498b36269d58
Pleum <thammasorn_m@cmu.ac.th>
2026-01-06 23:39:53 +0700

    1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int n);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+
+
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 }
